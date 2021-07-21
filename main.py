12ac8eb45a5fab74f9500c7e6c523d49fc60bc27
Ben Bauer <ben.bauer1@gmail.com>
2021-07-21 11:10:00 -0500

    UI created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,6 +13,8 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def timer_click():
+    pass
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -24,9 +27,18 @@ canvas = Canvas(width=205, height = 224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=tomato)
 canvas.create_text(103, 125, text="00:00", fill="white", font= (FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+label_timer = Label(text="Timer", font= (FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+label_timer.grid(column=1, row=0)
 
+button_start = Button(text="Start", command=timer_click)
+button_start.grid(column=0, row=2)
 
+button_stop = Button(text="Stop", command=timer_click)
+button_stop.grid(column=2, row=2)
+
+label_timer = Label(text="v", font= (FONT_NAME, 15, "bold"), fg=GREEN, bg=YELLOW)
+label_timer.grid(column=1, row=3)
 
 window.mainloop()
